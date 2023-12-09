d3ac1853fa2c570a20e3bd27d25195a5e1371ea7
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2023-12-09 17:02:17 +0100

    SvdDevice: Add documentation

--- src/main/java/io/svdparser/SvdDevice.java
+++ src/main/java/io/svdparser/SvdDevice.java
@@ -29,12 +29,28 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
+/**
+ * This class represents an SVD device. The element device provides the
+ * outermost frame of the description in an SVD file. A device contains one or
+ * more peripherals, but one CPU description.
+ */
 public class SvdDevice {
 
 	private SvdCpu mCpu;
 	private List<SvdPeripheral> mPeripherals;
 
-	public static SvdDevice fromFile(File f) throws SAXException, IOException, ParserConfigurationException, SvdParserException {
+	/**
+	 * Obtain a SvdDevice object directly from an SVD file.
+	 * 
+	 * @param f File to be read.
+	 * @return SvdDevice object.
+	 * @throws SAXException                 On XML parsing error.
+	 * @throws IOException                  On file operation error.
+	 * @throws ParserConfigurationException On XML parsing error.
+	 * @throws SvdParserException           On a SVD format error.
+	 */
+	public static SvdDevice fromFile(File f)
+			throws SAXException, IOException, ParserConfigurationException, SvdParserException {
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
 		DocumentBuilder db = dbf.newDocumentBuilder();
@@ -42,12 +58,26 @@ public class SvdDevice {
 		return fromDocument(doc);
 	}
 
+	/**
+	 * Obtain a SvdDevice object from a DOM document.
+	 * 
+	 * @param doc Document object.
+	 * @return SvdDevice object.
+	 * @throws SvdParserException On a SVD format error.
+	 */
 	public static SvdDevice fromDocument(Document doc) throws SvdParserException {
 		// http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-        doc.getDocumentElement().normalize();
-        return fromElement(doc.getDocumentElement());        
+		doc.getDocumentElement().normalize();
+		return fromElement(doc.getDocumentElement());
 	}
-	
+
+	/**
+	 * Obtain a SvdDevice object from a DOM element.
+	 * 
+	 * @param el Element object.
+	 * @return SvdDevice object.
+	 * @throws SvdParserException On a SVD format error.
+	 */
 	public static SvdDevice fromElement(Element el) throws SvdParserException {
 		if (!el.getNodeName().equals("device"))
 			throw new SvdParserException("Cannot build an SvdDevice from a " + el.getNodeName() + " node!");
@@ -61,7 +91,7 @@ public class SvdDevice {
 		List<SvdPeripheral> periphs = new ArrayList<>();
 		for (Element e : Utils.getFirstOrderChildElementsByTagName(peripheralsElement, "peripheral"))
 			periphs.add(SvdPeripheral.fromElement(e));
-		
+
 		// Return the new SVD device
 		return new SvdDevice(cpu, periphs);
 	}
@@ -71,10 +101,20 @@ public class SvdDevice {
 		mPeripherals = periphs;
 	}
 
+	/**
+	 * Get the CPU portion of a device.
+	 * 
+	 * @return An SvdCpu object.
+	 */
 	public SvdCpu getCpu() {
 		return mCpu;
 	}
-	
+
+	/**
+	 * Get the peripheral list of the device.
+	 * 
+	 * @return A list of SvdPeripheral objects.
+	 */
 	public List<SvdPeripheral> getPeripherals() {
 		return mPeripherals;
 	}
