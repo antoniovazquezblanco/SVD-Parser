eb692f9f644acfe20069a5440cc36de45ee8745c
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2023-12-09 17:02:38 +0100

    SvdParserException: Add documentation

--- src/main/java/io/svdparser/SvdParserException.java
+++ src/main/java/io/svdparser/SvdParserException.java
@@ -15,10 +15,17 @@
  */
 package io.svdparser;
 
+/**
+ * A runtime exception that may happen when parsing a SVD file.
+ */
 public class SvdParserException extends RuntimeException {
 
 	private static final long serialVersionUID = 7346114051394569394L;
 
+	/**
+	 * Create an SvdParserException from an error message.
+	 * @param string Error message.
+	 */
 	public SvdParserException(String string) {
 		super(string);
 	}
