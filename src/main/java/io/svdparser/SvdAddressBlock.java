c57384ca71e7036b917811c955602548235c5753
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2023-12-09 16:33:40 +0100

    SvdAddressBlock: Add documentation

--- src/main/java/io/svdparser/SvdAddressBlock.java
+++ src/main/java/io/svdparser/SvdAddressBlock.java
@@ -1,13 +1,30 @@
 package io.svdparser;
 
-
 import org.w3c.dom.Element;
 
+/**
+ * A class representing a CMSIS SVD Address Block element. This object specifies
+ * an address range uniquely mapped to a peripheral. A peripheral must have at
+ * least one address block. However, if a peripheral is derived form another
+ * peripheral, the address block is not mandatory.
+ *
+ * @see <a href=
+ *      "https://www.keil.com/pack/doc/CMSIS/SVD/html/elem_peripherals.html#elem_addressBlock">CMSIS
+ *      SVD Address Block documentation</a> for more information.
+ */
 public class SvdAddressBlock {
 	private Long mOffset;
 	private Long mSize;
 	private String mUsage;
 
+	/**
+	 * Construct an address block from a DOM element.
+	 *
+	 * @param el DOM element.
+	 * @return A SvdAddressBlock object.
+	 * @throws SvdParserException when the element does not correspond to an address
+	 *                            block.
+	 */
 	public static SvdAddressBlock fromElement(Element el) throws SvdParserException {
 		if (!el.getNodeName().equals("addressBlock"))
 			throw new SvdParserException("Cannot build an SvdAddressBlock from a " + el.getNodeName() + " node!");
@@ -33,14 +50,38 @@ public class SvdAddressBlock {
 		mUsage = usage;
 	}
 
+	/**
+	 * Returns address block usage description. The following predefined values can
+	 * be used:
+	 * <ul>
+	 * <li>registers</li>
+	 * <li>buffer</li>
+	 * <li>reserved</li>
+	 * </ul>
+	 *
+	 * @return Address block usage description.
+	 */
 	public String getUsage() {
 		return mUsage;
 	}
 
+	/**
+	 * Returns the start address of an address block relative to the peripheral
+	 * baseAddress.
+	 *
+	 * @return Offset of the address block.
+	 */
 	public Long getOffset() {
 		return mOffset;
 	}
 
+	/**
+	 * Returns the number of addressUnitBits being covered by this address block.
+	 * The end address of an address block results from the sum of baseAddress,
+	 * offset, and (size - 1).
+	 *
+	 * @return Size of the address block.
+	 */
 	public Long getSize() {
 		return mSize;
 	}
